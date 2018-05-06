f3ae9045f3bbbd1d8aee880ef52eab5d79be0f57
guilherme <ggregorio666@hotmail.com>
2018-05-06 09:53:07 -0300

    Alteração de arquivo de Bloking para DataSplit com implementações iniciais. Criação de partições como diretorios

---
+++ src/main/java/block/Partition.java
@@ -0,0 +1,16 @@
+package main.java.block;
+
+import java.io.File;
+
+public class Partition {
+
+    private String id;
+    private File aticles;
+    private File session;
+    private File recomendation;
+
+    public Partition(String id){
+        this.id = id;
+    }
+
+}
